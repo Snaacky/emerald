1e0419db54b14428c97fbc311d951ee3c01a1d75
Snaacky <snaacky@gmail.com>
2018-05-24 07:00:04 -0400

    Removed unnecessary check, added exceptions
    Added exceptions when invalid memory read/writes occur if Emerald is loaded before the player is alive in-game.
    
    Entity check was fundementally flawed/broken so removed.

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Emerald
-A no-flash cheat for CS:GO written in Python.
+No-flash cheat.
 
 ## Warning
 Emerald comes as is with no guarantees regarding its standing with VAC. This is a cheat and will most likely get you banned if you attempt to use it on any cheat protected servers. I have only tested Emerald on private servers with VAC disabled and recommend you do the same. I do not encourage cheating in any form and made this solely to learn more.
 
 ## Requirements
-* [Python 3.x](https://www.python.org/)
+* [Python >= 3.5](https://www.python.org/)
 * [Pymem](https://github.com/srounet/Pymem)
-* [keyboard](https://github.com/boppreh/keyboard)
 * [pywin32](https://sourceforge.net/projects/pywin32/files/?source=navbar)
--- emerald.py
+++ emerald.py
@@ -4,7 +4,6 @@ import time
 
 dwLocalPlayer = (0xAA9AB4)
 m_flFlashMaxAlpha = (0xA2F4)
-dwEntityList = (0x4A8473C)
 
 pm = pymem.Pymem("csgo.exe")
 client = pymem.process.module_from_name(pm.process_id, "client.dll").base_address
@@ -13,12 +12,15 @@ def main():
     print("Emerald has launched.")
 
     while True:
-        # check if player is in-game (if entity list exists)
-        if pm.read_int(client + dwEntityList) > 0:
+        try:
             player = pm.read_int(client + dwLocalPlayer)
             flash_value = player + m_flFlashMaxAlpha
             pm.write_float(flash_value, float(0))
-        time.sleep(0.002)
+            time.sleep(0.002)
+        except pymem.exception.MemoryReadError:
+            pass # Can happen if script loads before alive in-game.
+        except pymem.exception.MemoryWriteError:
+            pass # Same reason as above.
 
 if __name__ == '__main__':
     main()
