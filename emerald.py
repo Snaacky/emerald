5d3740490277bdacea96775dd830866b33d9f615
Snaacky <snaacky@gmail.com>
2018-09-15 09:04:26 -0400

    Updated for Panorama UI

--- emerald.py
+++ emerald.py
@@ -2,11 +2,12 @@ import pymem
 import pymem.process
 import time
 
-dwLocalPlayer = (0xAA9AB4)
-m_flFlashMaxAlpha = (0xA2F4)
+dwLocalPlayer = (0xC5E87C)
+m_flFlashMaxAlpha = (0xA304)
 
 pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_id, "client.dll").base_address
+client = pymem.process.module_from_name(pm.process_id, "client_panorama.dll").base_address
+
 
 def main():
     print("Emerald has launched.")
@@ -18,9 +19,10 @@ def main():
             pm.write_float(flash_value, float(0))
             time.sleep(0.002)
         except pymem.exception.MemoryReadError:
-            pass # Can happen if script loads before alive in-game.
+            pass  # Can happen if script loads before alive in-game.
         except pymem.exception.MemoryWriteError:
-            pass # Same reason as above.
+            pass  # Same reason as above.
+
 
 if __name__ == '__main__':
     main()
