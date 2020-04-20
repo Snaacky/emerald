1619dff6a1ee0639f2b9da0b15ab1c1addb5d7a2
Snaacky <Snaacky@pm.me>
2020-04-20 08:27:45 -0400

    fix: 🐛 Moved Pymem global->local space, more error prevention

--- emerald.py
+++ emerald.py
@@ -5,19 +5,19 @@ import time
 dwLocalPlayer = (0xD2FB84)
 m_flFlashMaxAlpha = (0xA40C)
 
-pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
-
 
 def main():
     print("Emerald has launched.")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 
     while True:
         player = pm.read_int(client + dwLocalPlayer)
-        flash_value = player + m_flFlashMaxAlpha
-        if flash_value:
-            pm.write_float(flash_value, float(0))
-            time.sleep(1)
+        if player:
+            flash_value = player + m_flFlashMaxAlpha
+            if flash_value:
+                pm.write_float(flash_value, float(0))
+                time.sleep(1)
 
 
 if __name__ == '__main__':
